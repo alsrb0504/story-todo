b0ce3d952d3d466e8d842986c38d765f9d1d16c6
MinKyu Tae <alsrb8442@naver.com>
2023-09-17 21:44:28 +0900

    설정: 투두리스트 프로젝트 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-orange-200 flex justify-center items-center h-screen">
+      <div className="h-5/6 w-1/2 bg-white py-4 text-center">
+        <h1 className="text-indigo-400 pb-4 text-xl font-bold border-b-default border-gray-300">
+          Todo List
+        </h1>
+      </div>
     </div>
   );
 }
