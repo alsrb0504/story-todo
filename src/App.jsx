84a08b80f01cf858682a6018228c4947fc990eb1
MinKyu Tae <alsrb8442@naver.com>
2023-09-17 18:47:53 +0900

    설정: 투두리스트 프로젝트 기본 설정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-orange-200 flex justify-center items-center h-screen">
+      <div className="h-5/6 w-1/2 bg-white py-4">
+        <h1 className="text-indigo-400 pb-4 text-xl font-bold border-b-default border-gray-300">
+          Todo List
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    borderWidth: {
+      default: "1px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
